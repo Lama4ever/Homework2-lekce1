550076b2b606e35d9a33d1fc8ef81d0f8dc2341e
Lama4ever <pychav@gmail.com>
2023-09-14 20:09:08 +0200

    2 domaci ukol

---
+++ src/com/engeto/homework2/CarrotSeller.java
@@ -0,0 +1,38 @@
+package com.engeto.homework2;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.time.LocalDate;
+
+public class CarrotSeller {
+    private String name;
+    private LocalDate birthDate;
+    private int numberOfInvoices;
+    //quantity in tons
+    private int soldQuantity;
+    private String city;
+    private String spz;
+    //in liters per 100 km
+    private BigDecimal consumption;
+    private String ip;
+
+    public CarrotSeller(String name, LocalDate birthDate, int numberOfInvoices, int soldQuantity, String city, String spz, BigDecimal consumption, String ip) {
+        this.name = name;
+        this.birthDate = birthDate;
+        this.numberOfInvoices = numberOfInvoices;
+        this.soldQuantity = soldQuantity;
+        this.city = city;
+        this.spz = spz;
+        this.consumption = consumption;
+        this.ip = ip;
+    }
+
+    public BigDecimal averageSellPerInvoice() {
+        BigDecimal average = BigDecimal.valueOf(soldQuantity).divide(BigDecimal.valueOf(numberOfInvoices), RoundingMode.HALF_DOWN);
+        return average;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
